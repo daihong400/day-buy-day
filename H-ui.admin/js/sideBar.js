087335ca1d942c695bda472c71689d0469009fdd
rodai <rodai@deloitte.com.cn>
2018-01-29 17:36:40 +0800

    aaa

---
+++ H-ui.admin/js/sideBar.js
@@ -0,0 +1,382 @@
+$(function(){
+    var dataMap=[
+        {
+            "id":"ba410aec-166c-469c-8164-52d07ff5852b",
+            "name":"\u98CE\u9669\u7BA1\u7406",
+            "desc":"\u5408\u89C4\u98CE\u9669\u81EA\u8BC4",
+            "forNavigation":true,
+            "companyId":"deloitte",
+            "url":null,
+            "icon":" url(>skin>common\/icons.gif) -220px -60px",
+            "parentId":null,
+            "order":null,
+            "systemId":null,
+            "use":false,
+            "target":null,
+            "hasChild":false,
+            "children":[
+                {
+                    "id":"3b1f8173-99fa-4d28-a92b-3348bac39c9a",
+                    "name":"\u5DE5\u4F5C\u6A21\u677F\u7BA1\u7406",
+                    "desc":"${res[\"risk.language\/menue_work_template_management\"]}",
+                    "forNavigation":true,
+                    "companyId":"deloitte",
+                    "url":null,
+                    "icon":" url(>skin>common\/icons.gif) -280px -60px",
+                    "parentId":"ba410aec-166c-469c-8164-52d07ff5852b",
+                    "order":null,
+                    "systemId":null,
+                    "use":false,
+                    "target":null,
+                    "hasChild":false,
+                    "children":[
+                        {
+                            "id":"d330d87a-f187-4115-bb48-6323cde0a4b6",
+                            "name":"\u5DE5\u4F5C\u6A21\u677F-\u521B\u5EFA",
+                            "desc":"${res[\"risk.language\/menue_work_template_add\"]}",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"com.deloitte.risk.rr.view.RRTemplateManager.ling",
+                            "icon":" url(>skin>common\/icons.gif) -260px -140px",
+                            "parentId":"3b1f8173-99fa-4d28-a92b-3348bac39c9a",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"e0cdb94c-9e15-48e8-8af1-5cbd3e8e93c7",
+                            "name":"\u5DE5\u4F5C\u6A21\u677F-\u4FEE\u8BA2",
+                            "desc":"${res[\"risk.language\/menue_work_template_modify\"]}",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"com.deloitte.risk.rr.view.RRTemplateModify.ling",
+                            "icon":" url(>skin>common\/icons.gif) -260px -140px",
+                            "parentId":"3b1f8173-99fa-4d28-a92b-3348bac39c9a",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "id":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                    "name":"\u98CE\u9669\u8BC4\u4F30",
+                    "desc":"\u98CE\u9669\u8BC4\u4F30",
+                    "forNavigation":true,
+                    "companyId":"deloitte",
+                    "url":null,
+                    "icon":" url(>skin>common\/icons.gif) -220px -140px",
+                    "parentId":"ba410aec-166c-469c-8164-52d07ff5852b",
+                    "order":null,
+                    "systemId":null,
+                    "use":false,
+                    "target":null,
+                    "hasChild":false,
+                    "children":[
+                        {
+                            "id":"625dfe15-3ba3-4fff-aaa8-99ea8654767e",
+                            "name":"\u5DE5\u4F5C\u8BA1\u5212-\u521B\u5EFA",
+                            "desc":"\u98CE\u9669\u8BC4\u4F30\u8BA1\u5212\u7BA1\u7406",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.plan.view.PlanMainInfoManager_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -0px -120px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"f20a497e-4825-4c40-a572-383231d5878e",
+                            "name":"\u5DE5\u4F5C\u8BA1\u5212-\u5BA1\u6279",
+                            "desc":null,
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.plan.view.PlanMainInfoApprove_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -20px -0px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"13b597b9-59be-48c2-821e-2eca973d2db9",
+                            "name":"\u5DE5\u4F5C\u8BA1\u5212-\u76D1\u63A7",
+                            "desc":null,
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.rcsa.view.RCSAPlanMainInfoMonitor_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -260px -60px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"3c46ddd4-d7b9-4db9-ac28-34d9b7bf5451",
+                            "name":"\u98CE\u9669\u8BC4\u4F30-\u6267\u884C",
+                            "desc":"\u5408\u89C4\u98CE\u9669\u81EA\u8BC4\u6267\u884C",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.rcsa.view.ExecuteSelfAssessmentRiskControl_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -300px -60px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"3581ddf1-88ce-41c0-9786-f88dc7060411",
+                            "name":"\u98CE\u9669\u8BC4\u4F30-\u5BA1\u6279",
+                            "desc":"\u5408\u89C4\u98CE\u9669\u81EA\u8BC4-\u5BA1\u6279",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.rcsa.view.ApproveSelfAssessmentRiskControl_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -300px -60px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"a0d29bae-5c63-4f51-be81-ad080de0fe1c",
+                            "name":"\u98CE\u9669\u8BC4\u4F30-\u8DDF\u8FDB",
+                            "desc":null,
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.rcsa.view.CheckSelfAssessmentRiskControl_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -60px -20px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"43014e89-58d7-4135-96fd-8a8bf29a590b",
+                            "name":"\u98CE\u9669\u8BC4\u4F30-\u67E5\u770B",
+                            "desc":null,
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.rcsa.view.ViewSelfAssessmentRiskControl_CITIC.ling",
+                            "icon":" url(>skin>common\/icons.gif) -20px -40px",
+                            "parentId":"35baa1dd-c851-4a89-a174-8cf4fa649baa",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "id":"751b5217-60f8-4f91-b734-62a1522123ec",
+                    "name":"\u91D1\u878D_\u4EA4\u6613\u5BF9\u624B\u62A5\u544A  ",
+                    "desc":null,
+                    "forNavigation":true,
+                    "companyId":"deloitte",
+                    "url":null,
+                    "icon":"  url(>skin>common\/icons.gif) -60px -160px",
+                    "parentId":"ba410aec-166c-469c-8164-52d07ff5852b",
+                    "order":null,
+                    "systemId":null,
+                    "use":false,
+                    "target":null,
+                    "hasChild":false,
+                    "children":[
+                    ]
+                }
+            ]
+        },
+        {
+            "id":"9f40826c-4c65-44ad-9e97-71a1613bf65b",
+            "name":"\u5408\u89C4\u7BA1\u7406",
+            "desc":null,
+            "forNavigation":true,
+            "companyId":"deloitte",
+            "url":null,
+            "icon":"url(>skin>common\/icons.gif) -60px -160px",
+            "parentId":null,
+            "order":null,
+            "systemId":null,
+            "use":false,
+            "target":null,
+            "hasChild":false,
+            "children":[
+                {
+                    "id":"7d9ec3ff-2358-4a00-bf8f-d473d3698e6c",
+                    "name":"\u5408\u89C4\u95EE\u7B54",
+                    "desc":"\u5408\u89C4\u95EE\u7B54",
+                    "forNavigation":true,
+                    "companyId":"deloitte",
+                    "url":null,
+                    "icon":" url(>skin>common\/icons.gif) -180px -0px",
+                    "parentId":"9f40826c-4c65-44ad-9e97-71a1613bf65b",
+                    "order":null,
+                    "systemId":null,
+                    "use":false,
+                    "target":null,
+                    "hasChild":false,
+                    "children":[
+                        {
+                            "id":"e7ec8931-d49d-4225-ad92-3a293f449adf",
+                            "name":"\u5408\u89C4\u63D0\u95EE",
+                            "desc":"",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.interlocution.view.InterlocutionManager.ling",
+                            "icon":" url(>skin>common\/icons.gif) -120px -60px",
+                            "parentId":"7d9ec3ff-2358-4a00-bf8f-d473d3698e6c",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"2e3c6969-04e7-432b-8787-bb37ebb65f9f",
+                            "name":"\u5408\u89C4\u56DE\u7B54",
+                            "desc":null,
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"com.deloitte.risk.interlocution.view.InterlocutionManagerAnswer.ling",
+                            "icon":" url(>skin>common\/icons.gif) -280px -60px",
+                            "parentId":"7d9ec3ff-2358-4a00-bf8f-d473d3698e6c",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        },
+                        {
+                            "id":"f811574d-7984-426c-90b1-79350cea6274",
+                            "name":"\u5408\u89C4\u6307\u6D3E",
+                            "desc":"\u5408\u89C4\u6307\u6D3E",
+                            "forNavigation":true,
+                            "companyId":"deloitte",
+                            "url":"risk.interlocution.view.InterlocutionManagerAssign.ling",
+                            "icon":" url(>skin>common\/icons.gif) -120px -120px",
+                            "parentId":"7d9ec3ff-2358-4a00-bf8f-d473d3698e6c",
+                            "order":null,
+                            "systemId":null,
+                            "use":false,
+                            "target":null,
+                            "hasChild":false,
+                            "children":[
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },{
+            "id":"a172a165-0d0d-4ae5-a489-dd3229101a0d",
+            "name":"\u6CD5\u5F8B\u6848\u4EF6\u7BA1\u7406",
+            "desc":"\u6848\u4EF6\u7BA1\u7406",
+            "forNavigation":true,
+            "companyId":"deloitte",
+            "url":null,
+            "icon":"url(>skin>common\/icons.gif) -60px -160px",
+            "parentId":null,
+            "order":null,
+            "systemId":null,
+            "use":false,
+            "target":null,
+            "hasChild":false,
+            "children":[
+            ]
+        },
+    ]
+
+    function initSideBar() {
+        $('#navMenu').html('');
+        var _html = '';
+        var _html2 = '';
+        var _hasChild1 = [];
+        $.each(dataMap,function(index,value){
+            if(value.children.length === 0) {
+                _html = '<li><a href="javascript:;" data-href="'+ value.url +'" class="afinve"><span class="">'+ value.name +'</span></a></li>';
+                $('#navMenu').append(_html);
+            }else {
+                _html = '<li><a href="javascript:;" class="afinve"> <span class="arrow"></span><span class="">'+ value.name +'</span></a></li>';
+                $('#navMenu').append(_html);
+                _hasChild1.push(index);
+
+                // 遍历第二层
+                $.each(value.children,function(index2,value2){
+                    if(index === 1) {
+                        return false;
+                    }
+                    console.log($('#navMenu').find('li:eq('+index+')'));
+                    console.log(value2.children.length)
+                    if(value2.children.length === 0) {
+                        _html2 = '<ul><li><a href="javascript:;" data-href="'+ value2.url +'" class="afinve"><span class="">'+ value2.name +'</span></a></li>';
+                        $('#navMenu').find('li:eq(index)').append(_html2);
+                    }else {
+                        _html2 = '<li><a href="javascript:;" class="afinve"> <span class="arrow"></span><span class="">'+ value2.name +'</span></a></li>';
+                        $('#navMenu').find('li:eq(index)').append(_html2);
+                    }
+                });
+
+            }
+
+        });
+
+        console.log(_hasChild1)
+
+    }
+    initSideBar();
+
+    function deepEach(data, elem) {
+        $.each(data, function(index,value){
+            console.log(index)
+            if(value.children.length === 0) {
+                _html = '<li><a href="javascript:;" data-href="'+ value.url +'" class="afinve"><span class="">'+ value.name +'</span></a></li>';
+                $('#navMenu').append(_html);
+
+            }else {
+                _html = '<li><a href="javascript:;" class="afinve"> <span class="arrow"></span><span class="">'+ value.name +'</span></a></li>';
+                $('#navMenu').append(_html);
+                _hasChild1.push(index);
+
+                deepEach(data.children, );
+            }
+        });
+    }
+}());
